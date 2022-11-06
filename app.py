671b121e279ca2df21fa9ffce9ca10140b2251d5
Arunangshu Das <darunangshu2002@gmail.com>
2022-11-06 10:00:43 +0530

    6th commit

--- app.py
+++ app.py
@@ -1,43 +1,149 @@
-import mediapipe as mp
 import cv2
 import numpy as np
+import mediapipe as mp
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 
-mp_drawing=mp.solutions.drawing_utils
-mp_pose=mp.solutions.pose
+def calculate_angle(a_ang, b_ang, c_ang):
+    a_ang = np.array(a_ang)
+    b_ang = np.array(b_ang)
+    c_ang = np.array(c_ang)
 
-def calculate_angle(a,b,c):
-    a=np.array(a)
-    b=np.array(b)
-    c=np.array(c)
+    radians = np.arctan2(c_ang[1] - b_ang[1], c_ang[0] - b_ang[0]) - np.arctan2(a_ang[1] - b_ang[1], a_ang[0] - b_ang[0])
+    angle = np.abs(radians * 180 / np.pi)
 
-    radians=np.arctan2(c[1]-b[1],c[0]-b[0])-np.arctan2(a[1]-b[1],a[0]-b[0])
-    angle=np.abs(radians*180/np.pi)
-    if(angle>180):
-        angle=360-angle
+    if (angle > 180.0):
+        angle = 360 - angle
+
     return angle
 
-left_hand_side = 0
-right_hand_side = 0
+# initialize mediapipe
+mpHands = mp.solutions.hands
+mp_pose=mp.solutions.pose
+hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
+mp_drawing=mp.solutions.drawing_utils
+
+# Load the gesture recognizer model
+model = load_model('mp_hand_gesture')
+
+# Load class names
+f = open('gesture.names', 'r')
+classNames = f.read().split('\n')
+f.close()
+print(classNames)
+
+counter = 0
+left_stage = None
+right_stage = None
+
+# Initialize the webcam
+cap = cv2.VideoCapture(0)
+
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        # make image to RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        x, y, c = frame.shape
+
+        # detection
+        results = pose.process(image)
+
+        # Recolor to BGR
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        # Extract landmark
+        try:
+            landmarks = results.pose_landmarks.landmark
+            # getcoordinates
+            left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+                             landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            left_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
+                          landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+            left_wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+                          landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+
+            left_angle = calculate_angle(left_shoulder, left_elbow, left_wrist)
+
+            # visualization
+            cv2.putText(image, str(left_angle), tuple(np.multiply(left_elbow, [640, 480]).astype(int)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+
+            right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
+                              landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+            right_elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,
+                           landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+            right_wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,
+                           landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+
+            right_angle = calculate_angle(right_shoulder, right_elbow, right_wrist)
+
+            # visualization
+            cv2.putText(image, str(right_angle), tuple(np.multiply(right_elbow, [640, 480]).astype(int)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+
+            #             print(landmarks)
+            if left_angle > 160:
+                left_stage = "down"
+            if left_angle < 30 and left_stage == 'down':
+                left_stage = 'up'
+                counter += 1
+            if right_angle > 160:
+                right_stage = "down"
+            if right_angle < 30 and right_stage == 'down':
+                right_stage = 'up'
+                counter += 1
+        except:
+            pass
+
+        result = hands.process(image)
+        className = ''
+
+        # post process the result
+        if result.multi_hand_landmarks:
+            landmarks = []
+            for handslms in result.multi_hand_landmarks:
+                for lm in handslms.landmark:
+                    # print(id, lm)
+                    lmx = int(lm.x * x)
+                    lmy = int(lm.y * y)
+
+                    landmarks.append([lmx, lmy])
+
+                # Drawing landmarks on frames
+                mp_drawing.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
+
+                # Predict gesture
+                prediction = model.predict([landmarks])
+                # print(prediction)
+                classID = np.argmax(prediction)
+                className = classNames[classID]
+
+        cv2.rectangle(image, (0, 0), (700, 73), (0, 0, 255), -1)
+        cv2.putText(image, 'REPS', (15, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+        cv2.putText(image, str(counter), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+        cv2.putText(image, 'LEFT_STAGE', (95, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+        cv2.putText(image, left_stage, (90, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+        cv2.putText(image, 'RIGHT_STAGE', (280, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+        cv2.putText(image, right_stage, (270, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+        cv2.putText(image, className, (465, 50), cv2.FONT_HERSHEY_SIMPLEX, 
+                   1, (255, 255, 255), 2, cv2.LINE_AA)
+
+        # Render detection
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                  mp_drawing.DrawingSpec(color=(255, 204, 204), thickness=1, circle_radius=3),
+                                  # dots color
+                                  mp_drawing.DrawingSpec(color=(102, 0, 0), thickness=2,
+                                                         circle_radius=2))  # connection color
 
 
-
-cap=cv2.VideoCapture(0)
-while cap.isOpened():
-    _,frame=cap.read()
-    cv2.imshow('Detection model',frame)
-    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    image.flags.writeable = False
-    results=pose.process()
-
-    image.flags.writeable=True
-    image=cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
-    
-
-    #Pose detection
-
-
-    if cv2.waitKey(10) & 0XFF==ord('q'):
-        break
-
+        cv2.imshow('Arunangshu Das', image)
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
 cap.release()
 cv2.destroyAllWindows()
